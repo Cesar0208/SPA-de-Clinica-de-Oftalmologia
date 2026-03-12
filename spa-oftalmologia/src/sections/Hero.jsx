ffe45815fa5a179758a439e2c56db000f5a100b5
CésarL <107433989+Cesar0208@users.noreply.github.com>
2026-03-12 15:12:04 -0300

    hero

---
+++ spa-oftalmologia/src/sections/Hero.jsx
@@ -0,0 +1,11 @@
+function Hero() {
+    return (
+        <section className="hero">
+            <div className="container hero-conteudo">
+
+            </div>
+        </section>
+    )
+}
+
+export default Hero;
