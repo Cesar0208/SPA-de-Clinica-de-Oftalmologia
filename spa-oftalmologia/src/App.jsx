175d265ce4a5486a6b905b792f65503a419d65f1
CésarL <107433989+Cesar0208@users.noreply.github.com>
2026-03-12 16:52:05 -0300

    Depoimentos

--- spa-oftalmologia/src/App.jsx
+++ spa-oftalmologia/src/App.jsx
@@ -1,6 +1,7 @@
 // Colocar os imports
 import Hero from "./sections/Hero";
 import Profissionais from "./sections/Profissionais";
+import Depoimentos from "./sections/Depoimentos";
 import Contato from "./sections/Contato";
 import Footer from "./sections/Footer";
 
@@ -10,6 +11,7 @@ export default function App() {
     <>
     <Hero />
     <Profissionais />
+    <Depoimentos />
     <Contato />
     <Footer />
     </>
