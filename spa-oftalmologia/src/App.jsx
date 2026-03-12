cb1ea752694faf5917b6793230ddc1f7ae44f709
Pedro Matos <pmattos.games@gmail.com>
2026-03-12 16:26:52 -0300

    ContatoeFooter

--- spa-oftalmologia/src/App.jsx
+++ spa-oftalmologia/src/App.jsx
@@ -1,9 +1,15 @@
 // Colocar os imports
+import React from 'react';
+import Footer from './sections/Footer';
+import Contato from './sections/Contato';
 
 function App() {
 
   return (
     <>
+      {/* outras seções do site podem ir aqui */}
+      <Contato />
+      <Footer />
     </>
   )
 }
