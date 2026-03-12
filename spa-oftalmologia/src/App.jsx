383d2e0748a59a995bf1c3bfbcea0987518a7cd8
CésarL <107433989+Cesar0208@users.noreply.github.com>
2026-03-12 16:43:56 -0300

    .

--- spa-oftalmologia/src/App.jsx
+++ spa-oftalmologia/src/App.jsx
@@ -1,9 +1,17 @@
 // Colocar os imports
+import Hero from "./sections/Hero";
+import Profissionais from "./sections/Profissionais";
+import Contato from "./sections/Contato";
+import Footer from "./sections/Footer";
 
 export default function App() {
 
   return (
     <>
+    <Hero />
+    <Profissionais />
+    <Contato />
+    <Footer />
     </>
   )
 }
